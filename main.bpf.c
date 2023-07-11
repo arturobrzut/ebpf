e47ef2c20512fa21f31845eea60e19d83051bfd9
Artur Obrzut <artur.obrzut@pl.ibm.com>
2023-07-11 19:55:28 +0200

    Update main.bpf.c

--- main.bpf.c
+++ main.bpf.c
@@ -2,6 +2,35 @@
 #include <vmlinux.h>
 #include <bpf/bpf_helpers.h>
 
+static inline u32 bpf_strlen(char *s)
+{
+    u32 i;
+    for (i = 0; s[i] != '\0' && i < (1 << (32 - 1)); i++);
+    return i;
+}
+
+static inline int bpf_strncmp(char *s1, char *s2, u32 n)
+{
+    int mismatch = 0;
+    for (int i = 0; i < n && i < sizeof(s1) && i < sizeof(s2); i++)
+    {
+        if (s1[i] != s2[i])
+            return s1[i] - s2[i];
+
+        if (s1[i] == s2[i] == '\0')
+            return 0;
+    }
+
+    return 0;
+}
+
+static inline int bpf_strcmp(char *s1, char *s2)
+{
+    u32 s1_size = sizeof(s1);
+    u32 s2_size = sizeof(s2);
+
+    return bpf_strncmp(s1, s2, s1_size < s2_size ? s1_size : s2_size);
+}
 
 SEC("kprobe/sys_read")
 int bpf_prog(struct pt_regs *ctx)
