a17638b76013028e810159f1cc814d6eff263c6e
Artur Obrzut <artur.obrzut@pl.ibm.com>
2024-02-25 13:20:27 +0100

    Update main.bpf.c

--- main.bpf.c
+++ main.bpf.c
@@ -1,56 +1,5 @@
-
 #include <vmlinux.h>
 #include <bpf/bpf_helpers.h>
-
-
-
-static inline int bpf_strcmp(char *s1, char *s2)
-{
-    u32 s1_size = sizeof(s1);
-    u32 s2_size = sizeof(s2);
-
-    return bpf_strncmp(s1, s2, s1_size < s2_size ? s1_size : s2_size);
-}
-
-SEC("kprobe/sys_open")
-int bpf_prog(struct pt_regs *ctx)
-{
-    struct filename *file;
-    char filename[256];
-bpf_printk("AAA %s", &(ctx->di));
-    bpf_printk("BBB %s", ctx->di);
-   //int ret =  bpf_probe_read_user_str(filename, sizeof(filename), ctx->di);
-   // bpf_printk("CODE %d", ret);
-//bpf_printk("XXX %s", filename);
-    
-    //bpf_probe_read_kernel(&file, sizeof(file), (void *)((ctx->di) + sizeof(unsigned long)));
-
-   // if (file) {
-     //   char path[128];
-       // bpf_probe_read_user_str(path, sizeof(path), (void *)file->name);
-
-      //  if (bpf_strcmp(path, "/home/test.txt") == 0) {
-        //    return -1; // Block the read
-       // }
-    //}
-
-    return 0; // Allow the read
-}
-
-SEC("kprobe/sys_read")
-int block_read(struct pt_regs *ctx)
-{
-    // Check if the file descriptor matches the one you want to block
-    int fd = 1; // ctx->di;
-    if (fd == 12) {
-        // Block the read system call by returning an error code
-        return -2; //-EPERM;
-    }
-    // Allow the read system call to proceed
-    return 0;
-}
-
-
 // Based on sudo cat /sys/kernel/debug/tracing/events/syscalls/sys_enter_fchmodat/format
 struct fchmodat_args {
     short common_type;
@@ -63,15 +12,12 @@ struct fchmodat_args {
     char *filename;
     int mode2;
 };
-
 struct {
     __uint(type, BPF_MAP_TYPE_ARRAY);
     __type(key, u32);
     __type(value, char[64]);
     __uint(max_entries, 64);
 } tech_talk SEC(".maps");
-
-
 SEC("tracepoint/syscalls/sys_enter_openat")
 int hello_tech_talk(struct fchmodat_args *ctx)
 {
@@ -83,7 +29,6 @@ int hello_tech_talk(struct fchmodat_args *ctx)
     char sendOK[64];
     char *tmpData;
     char *tmpData2;
-
     // GET FILENAME WHICH SOMEONE O PEN(FROM HOOK)
     ret = bpf_probe_read(&tmpData, sizeof(tmpData), &ctx->filename);
     if (ret != 0) {
@@ -100,17 +45,14 @@ int hello_tech_talk(struct fchmodat_args *ctx)
     if (!filename) {
         bpf_printk("ERROR Read problem with configmap");
     }
-
     ret = bpf_probe_read(&tmpData2, sizeof(tmpData2), &filename);
     if (ret != 0) {
         bpf_printk("ERROR Read2");
     }
-
     ret = bpf_probe_read_str(valMyFileName, sizeof(valMyFileName), tmpData2);
     if (ret < 0) {
         bpf_printk("ERROR Read String2");
     }
-
     if (compare(valKernelFileName, valMyFileName, 11) == 0) {
         // UPDATE MAP FOR USER SPACE PROGRAM
         ret = bpf_map_update_elem(&tech_talk, &keyOutput, &valMyFileName, BPF_ANY);
@@ -120,14 +62,11 @@ int hello_tech_talk(struct fchmodat_args *ctx)
         bpf_printk("Found unauthorized open my file, kill the process");
         bpf_printk("FileName: %s ",valKernelFileName);
         bpf_printk("PID: %d",bpf_get_current_pid_tgid());
-
         //KILL THE PROCESS WHO OPEN MY FILE
         bpf_send_signal(9);
     }
     return 0;
 }
-
-
 int compare(char src[64], char dst[64], int sizeVal) {
   int retVal = 0;
   for (int index=0;index<sizeVal;index++) {
@@ -138,5 +77,4 @@ int compare(char src[64], char dst[64], int sizeVal) {
   }
   return retVal;
 }
-
 char LICENSE[] SEC("license") = "GPL";
